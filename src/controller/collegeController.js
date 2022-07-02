b0ffbe321e7ed37882053cf97a578edfa6b14e1f
adiyadav0 <adiyadav596@gmail.com>
2022-07-02 20:13:50 +0530

    Project2

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -1,14 +1,11 @@
 const collegeModel = require("../models/collegeModel");
 const internModel = require("../models/internModel");
-const valid = require("../validation/validation")
-
-
-
+const valid = require("../validation/validation");
 
 const college = async function (req, res) {
   try {
     let collegeData = req.body;
-    let {name, fullName, logoLink} = req.body
+    let { name, fullName, logoLink } = req.body;
 
     if (Object.keys(collegeData).length == 0) {
       return res
@@ -16,15 +13,13 @@ const college = async function (req, res) {
         .send({ status: false, msg: "Body should  be not Empty.. " });
     }
 
-    
+    //<-------These validations for Mandatory fields--------->//
 
-   //<-------These validations for Mandatory fields--------->//
-     if(!valid.isValid(name)){ 
-        return res
-           .status(400)
-           .send({ status: false, msg: "Name field is mandatory" });
-      }
-      
+    if (!valid.isValid(name)) {
+      return res
+        .status(400)
+        .send({ status: false, msg: "Name field is mandatory" });
+    }
 
     if (!valid.isValid(fullName)) {
       return res
@@ -34,12 +29,11 @@ const college = async function (req, res) {
 
     if (!valid.isValid(logoLink)) {
       return res
-      .status(400)
-      .send({ status: false, msg: "LogoLink filed is mandatory" });
+        .status(400)
+        .send({ status: false, msg: "LogoLink filed is mandatory" });
     }
 
-
-    name = name.toLowerCase()
+    name = name.toLowerCase();
     let duplicateName = await collegeModel.findOne({ name: name });
     if (duplicateName) {
       return res
@@ -47,25 +41,33 @@ const college = async function (req, res) {
         .send({ status: false, msg: "Name Already Exist." });
     }
 
-    collegeData.name = name
+    collegeData.name = name;
 
-   //<--------These ensure that name and fullName in Alphabets------->//
+    //<--------These ensure that name and fullName in Alphabets------->//
     if (!valid.reg(name))
       return res
         .status(400)
         .send({ status: false, msg: "Please Use only Alphabets in name" });
 
-   
     if (!valid.reg(fullName))
       return res
         .status(400)
         .send({ status: false, msg: "Please Use only Alphabets in full name" });
 
-     let reg=  /^(ftp|http|https):\/\/[^ "]+$/
-     if(!reg.test(logoLink)) return res.status(400).send({status:false, message:"Please provide a valid url."})
+    collegeData.name = name.replace(/\s+/g, " ");
+    collegeData.fullName = fullName.replace(/\s+/g, " ");
 
-     let regex = /^https?:\/\/.*\/.*\.(png|gif|webp|jpeg|jpg)\??.*$/gmi
-     if(!regex.test(logoLink)) return res.status(400).send({status : false, msg : "Please Provide valid extension." })
+    let reg = /^(ftp|http|https):\/\/[^ "]+$/;
+    if (!reg.test(logoLink))
+      return res
+        .status(400)
+        .send({ status: false, message: "Please provide a valid url." });
+
+    let regex = /^https?:\/\/.*\/.*\.(png|gif|webp|jpeg|jpg)\??.*$/gim;
+    if (!regex.test(logoLink))
+      return res
+        .status(400)
+        .send({ status: false, msg: "Please Provide valid extension." });
 
     let result = await collegeModel.create(collegeData);
     res.status(201).send({ status: true, Data: result });
@@ -74,29 +76,34 @@ const college = async function (req, res) {
   }
 };
 
-
 const getColleges = async function (req, res) {
   try {
     let college = req.query.collegeName;
     if (Object.keys(req.query).length == 0) {
       return res
-      .status(400)
-      .send({ status: false, msg: "CollegeName is Mandatory." });
+        .status(400)
+        .send({ status: false, msg: "CollegeName is Mandatory." });
     }
-     
-    if(!college) return res.status(400).send({status :false , msg : "Enter College Name."})
-    if (!valid.reg(college)) return res.status(400).send({status: false, msg: "Use Alphabets only."})
 
-    college = college.toLowerCase()
+    if (!college)
+      return res
+        .status(400)
+        .send({ status: false, msg: "Enter College Name." });
+    if (!valid.reg(college))
+      return res
+        .status(400)
+        .send({ status: false, msg: "Use Alphabets only." });
 
- 
-    
+    college = college.toLowerCase();
+
     let result = await collegeModel
       .findOne({ name: college })
       .select({ name: 1, fullName: 1, logoLink: 1, _id: 1 });
-    
-    if(!result){
-      return res.status(404).send({status : false, msg : "College Does Not Exist"})
+
+    if (!result) {
+      return res
+        .status(404)
+        .send({ status: false, msg: "College Does Not Exist" });
     }
 
     let collegeId = result._id.toString();
@@ -105,14 +112,14 @@ const getColleges = async function (req, res) {
       .find({ collegeId: collegeId })
       .select({ name: 1, email: 1, mobile: 1 });
 
-    if(interns.length == 0){
-      return res.status(404).send({status:false, msg: "No intern exist."})
+    if (interns.length == 0) {
+      return res.status(404).send({ status: false, msg: "No intern exist." });
     }
 
     let name = result.name;
     let fullName = result.fullName;
     let logoLink = result.logoLink;
-    
+
     let internDetails = {
       name: name,
       fullName: fullName,
@@ -130,4 +137,3 @@ module.exports = {
   college: college,
   getColleges: getColleges,
 };
-
